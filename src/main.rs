15921585c0b51b5a34820bdeea2be9683633fe5d
Caio Gomes <caiocodes@proton.me>
2024-11-11 11:38:02 +0000

    add env control

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use azure_core::Url;
+use azure_storage::StorageCredentials;
 use azure_storage_blobs::prelude::*;
 use futures::stream::StreamExt;
+use std::env;
 
 #[tokio::main]
 async fn main() -> azure_core::Result<()> {
-    let blob_service_client = ClientBuilder::emulator().blob_service_client();
+    dotenvy::dotenv().unwrap();
+
+    let blob_service_client = match env::var("env") {
+        Ok(env) => match env.as_str() {
+            "azurite" => ClientBuilder::emulator().blob_service_client(),
+            "azure" => {
+                let account_name =
+                    env::var("AZURE_STORAGE_ACCOUNT_NAME").expect("Missing env for account name");
+                let account_key =
+                    env::var("AZURE_STORAGE_ACCESS_KEY").expect("Missing env for account key");
+
+                let credential = StorageCredentials::access_key(&account_name, account_key);
+
+                BlobServiceClient::new(&account_name, credential)
+            }
+            _ => panic!("Invalid env"),
+        },
+        Err(_) => panic!("env not found"),
+    };
 
     let mut container_client = blob_service_client.list_containers().into_stream();
 
